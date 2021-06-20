4af296763fb0c80398ddd89daaaf14244fbe17db
mohammad <mohammadeng3731@gmail.com>
2021-06-20 05:25:03 -0700

    choose component -> about component with 3 cart fixed

--- components/choose/index.vue
+++ components/choose/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div dir="rtl">
+    <div >
         <div class="choose-area pt-100 pb-70">
             <div class="choose-shape">
                 <img src="@/assets/img/home-one/choose1.png" alt="Shape">
@@ -13,42 +13,45 @@
                             <div class="row align-items-center">
                                 <div class="col-sm-6 col-lg-6">
                                     <div class="col-lg-12 p-0">
-                                        <div class="choose-item">
-                                            <i class='bx bx-network-chart'></i>
-                                            <h3>ما به شما خدمات با کیفیتی ارائه می دهیم</h3>
-                                            <p>لورم ایپسوم ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم استاندارد صنعت بوده است.</p>
-                                        </div>
+                                        <Card />
                                     </div>
                                     <div class="col-lg-12 p-0">
-                                        <div class="choose-item">
+                                        <!-- <div class="choose-item">
                                             <i class='bx bx-paper-plane'></i>
                                             <h3>پاسخ سریع و پشتیبانی 24/7</h3>
                                             <p>لورم ایپسوم ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم استاندارد صنعت بوده است.</p>
-                                        </div>
+                                        </div> -->
+                                        <Card />
+
                                     </div>
                                 </div>
                                 <div class="col-sm-6 col-lg-6">
-                                    <div class="choose-item">
+                                    <!-- <div class="choose-item">
                                         <i class='bx bx-wrench'></i>
                                         <h3>بکارگیری از تجهیزات مدرن و بروز دنیا</h3>
                                         <p>لورم ایپسوم ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم استاندارد صنعت بوده است.</p>
-                                    </div>
+                                    </div> -->
+                                        <Card />
+
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="choose-contact">
-                            <div class="section-title">
-                                <span class="sub-title">چرا ما را انتخاب می کنید</span>
-                                <h2>شرکت خدمات نظافتی مجاز بیش از 15 سال تجربه</h2>
-                            </div>
-                            <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                            <a class="cmn-btn" href="contact.html">تماس با ما</a>
-                        </div>
+                    <div class="col-lg-6" >
+                        <WhyUs />
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+import Card from "./card"
+import WhyUs from "./whyus"
+export default {
+ components:{
+     Card,
+     WhyUs
+ }   
+}
+</script>
---
+++ components/choose/whyus/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div dir="rtl">
+        <div class="choose-contact" >
+            <div class="section-title" style="text-align:right">
+                <span class="sub-title">چرا ما را انتخاب می کنید</span>
+                <h2>شرکت خدمات نظافتی مجاز بیش از 15 سال تجربه</h2>
+            </div>
+            <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+            <a class="cmn-btn" href="contact.html">تماس با ما</a>
+        </div>
+    </div>    
+</template>
