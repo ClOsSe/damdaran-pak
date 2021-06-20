d6aea21e24f38c833806d6dbcb63d13c0741680d
mohammad <mrg2195@gmail.com>
2021-06-20 06:50:22 -0700

    register order form and subscribe components added to the register order page

--- pages/registerOrder/index.vue
+++ pages/registerOrder/index.vue
@@ -30,71 +30,13 @@
             </div>
         </div>
         <!-- End Page Title -->
-
-        <div class="estimate-area ptb-100 mt-5">
-            <div class="container">
-                <div class="estimate-content">
-                    
-                    <form>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="نام شما">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <input type="number" class="form-control" placeholder="شماره شما">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="آدرس شما">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="موضوع">
-                                </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <div class="form-group">
-                                    <textarea id="your-message" rows="6" class="form-control" placeholder="پیام شما"></textarea>
-                                </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <button type="submit" class="btn cmn-btn">دریافت برنامه</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div>
+            <RegisterOrderForm />
         </div>
+        
 
         <!-- Subscribe -->
-        <div class="subscribe-area">
-            <div class="subscribe-wrap">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <div class="section-title">
-                                <h2>در خبرنامه ما عضو شوید</h2>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <form class="newsletter-form" data-toggle="validator">
-                                <input type="email" class="form-control" placeholder="آدرس ایمیل شما" name="EMAIL" required autocomplete="off">
-        
-                                <button class="btn cmn-btn" type="submit">
-                                    مشترک شدن
-                                </button>
-                                <div id="validator-newsletter" class="form-result"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Subscribe />
         <!-- End Subscribe -->
 
    
@@ -102,9 +44,13 @@
 </template>
 <script>
 import Loader from '@/components/loader'
+import RegisterOrderForm from '@/components/registerOrderForm'
+import Subscribe from '@/components/subscribe'
 export default {
     components:{
-        Loader
+        Loader,
+        RegisterOrderForm,
+        Subscribe
     }
 }
 </script>
