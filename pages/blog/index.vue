73a11b2ded17f7c6ed765df3986fb8caaabc4875
mohammad <mrg2195@gmail.com>
2021-06-20 07:35:31 -0700

    blogPost component created and aded to the blog pages

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<div dir="rtl">	
          <!-- Preloader -->
-        <div class="loader">
-            <Loader />
-        </div>
+        <!-- <div class="loader"> -->
+            <!-- <Loader /> -->
+        <!-- </div> -->
         <!-- End Preloader -->
 
         <!-- Page Title -->
@@ -36,190 +36,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-lg-4">
-                        <div class="blog-item">
-                            <NuxtLink to="/blogDetails">
-                                <img src="@/assets/img/home-three/blog1.jpg" alt="Blog">
-                            </NuxtLink>
-                            <div class="blog-bottom">
-                                <h3>
-                                    <NuxtLink to="/blogDetails">آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی</NuxtLink>
-                                </h3>
-                                <ul>
-                                    <li>
-                                        <span>توسط:</span>
-                                        <a href="#">مدیر</a>
-                                    </li>
-                                    <li>
-                                        <span>20</span>
-                                        <a href="#">نظرات</a>
-                                    </li>
-                                    <li>
-                                        <span>11</span>
-                                        <a href="#">مشاهده</a>
-                                    </li>
-                                </ul>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                                <a href="blog-details.html">
-                                    بیشتر بخوانید
-                                    <i class='bx bx-left-arrow-alt'></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="blog-item">
-                            <a href="blog-details.html">
-                                <img src="@/assets/img/home-three/blog2.jpg" alt="Blog">
-                            </a>
-                            <div class="blog-bottom">
-                                <h3>
-                                    <a href="blog-details.html">آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی</a>
-                                </h3>
-                                <ul>
-                                    <li>
-                                        <span>توسط:</span>
-                                        <a href="#">مدیر</a>
-                                    </li>
-                                    <li>
-                                        <span>20</span>
-                                        <a href="#">نظرات</a>
-                                    </li>
-                                    <li>
-                                        <span>11</span>
-                                        <a href="#">مشاهده</a>
-                                    </li>
-                                </ul>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                                <a href="blog-details.html">
-                                    بیشتر بخوانید
-                                    <i class='bx bx-left-arrow-alt'></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="blog-item">
-                            <a href="blog-details.html">
-                                <img src="@/assets/img/home-three/blog3.jpg" alt="Blog">
-                            </a>
-                            <div class="blog-bottom">
-                                <h3>
-                                    <a href="blog-details.html">آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی</a>
-                                </h3>
-                                <ul>
-                                    <li>
-                                        <span>توسط:</span>
-                                        <a href="#">مدیر</a>
-                                    </li>
-                                    <li>
-                                        <span>20</span>
-                                        <a href="#">نظرات</a>
-                                    </li>
-                                    <li>
-                                        <span>11</span>
-                                        <a href="#">مشاهده</a>
-                                    </li>
-                                </ul>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                                <a href="blog-details.html">
-                                    بیشتر بخوانید
-                                    <i class='bx bx-left-arrow-alt'></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="blog-item">
-                            <a href="blog-details.html">
-                                <img src="@/assets/img/home-three/blog4.jpg" alt="Blog">
-                            </a>
-                            <div class="blog-bottom">
-                                <h3>
-                                    <a href="blog-details.html">آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی</a>
-                                </h3>
-                                <ul>
-                                    <li>
-                                        <span>توسط:</span>
-                                        <a href="#">مدیر</a>
-                                    </li>
-                                    <li>
-                                        <span>20</span>
-                                        <a href="#">نظرات</a>
-                                    </li>
-                                    <li>
-                                        <span>11</span>
-                                        <a href="#">مشاهده</a>
-                                    </li>
-                                </ul>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                                <a href="blog-details.html">
-                                    بیشتر بخوانید
-                                    <i class='bx bx-left-arrow-alt'></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="blog-item">
-                            <a href="blog-details.html">
-                                <img src="@/assets/img/home-three/blog5.jpg" alt="Blog">
-                            </a>
-                            <div class="blog-bottom">
-                                <h3>
-                                    <a href="blog-details.html">آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی</a>
-                                </h3>
-                                <ul>
-                                    <li>
-                                        <span>توسط:</span>
-                                        <a href="#">مدیر</a>
-                                    </li>
-                                    <li>
-                                        <span>20</span>
-                                        <a href="#">نظرات</a>
-                                    </li>
-                                    <li>
-                                        <span>11</span>
-                                        <a href="#">مشاهده</a>
-                                    </li>
-                                </ul>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                                <a href="blog-details.html">
-                                    بیشتر بخوانید
-                                    <i class='bx bx-left-arrow-alt'></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="blog-item">
-                            <a href="blog-details.html">
-                                <img src="@/assets/img/home-three/blog6.jpg" alt="Blog">
-                            </a>
-                            <div class="blog-bottom">
-                                <h3>
-                                    <a href="blog-details.html">آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی</a>
-                                </h3>
-                                <ul>
-                                    <li>
-                                        <span>توسط:</span>
-                                        <a href="#">مدیر</a>
-                                    </li>
-                                    <li>
-                                        <span>20</span>
-                                        <a href="#">نظرات</a>
-                                    </li>
-                                    <li>
-                                        <span>11</span>
-                                        <a href="#">مشاهده</a>
-                                    </li>
-                                </ul>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
-                                <a href="blog-details.html">
-                                    بیشتر بخوانید
-                                    <i class='bx bx-left-arrow-alt'></i>
-                                </a>
-                            </div>
-                        </div>
+                        <BlogPost />
                     </div>
                 </div>
                 <div class="pagination-area">
@@ -246,153 +63,21 @@
         <!-- End Blog -->
 
         <!-- Subscribe -->
-        <div class="subscribe-area">
-            <div class="subscribe-wrap">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <div class="section-title">
-                                <h2>در خبرنامه ما عضو شوید</h2>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <form class="newsletter-form" data-toggle="validator">
-                                <input type="email" class="form-control" placeholder="آدرس ایمیل شما" name="EMAIL" required autocomplete="off">
-        
-                                <button class="btn cmn-btn" type="submit">
-                                    مشترک شدن
-                                </button>
-                                <div id="validator-newsletter" class="form-result"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Subscribe />
         <!-- End Subscribe -->
-
-        <!-- Footer -->
-        <footer class="pt-100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-item">
-                            <div class="footer-logo">
-                                <a href="index.html">
-                                    <img src="@/assets/img/logo.png" alt="Logo">
-                                </a>
-                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-item">
-                            <div class="footer-company">
-                                <h3>شرکت</h3>
-                                <ul>
-                                    <li>
-                                        <a href="about.html" target="_blank">درباره ما</a>
-                                    </li>
-                                    <li>
-                                        <a href="services.html" target="_blank">خدمات</a>
-                                    </li>
-                                    <li>
-                                        <a href="projects.html" target="_blank">پروژه ها</a>
-                                    </li>
-                                    <li>
-                                        <a href="team.html" target="_blank">تیم ما</a>
-                                    </li>
-                                    <li>
-                                        <a href="blog.html" target="_blank">وبلاگ</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-item">
-                            <div class="footer-company">
-                                <h3>پشتیبانی</h3>
-                                <ul>
-                                    <li>
-                                        <a href="faq.html" target="_blank">سوالات متداول</a>
-                                    </li>
-                                    <li>
-                                        <a href="privacy-policy.html" target="_blank">حریم خصوصی</a>
-                                    </li>
-                                    <li>
-                                        <a href="terms-and-conditions.html" target="_blank">شرایط و ضوابط</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" target="_blank">انجمن</a>
-                                    </li>
-                                    <li>
-                                        <a href="contact.html" target="_blank">تماس با ما</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-item">
-                            <div class="footer-contact">
-                                <h3>اطلاعات تماس</h3>
-                                <ul>
-                                    <li>
-                                        <span>موقعیت: ایران ، استان تهران ، میدان آزادی</span>
-                                    </li>
-                                    <li>
-                                        <span>ایمیل:</span>
-                                        <a href="mailto:info@lixi.com" target="_blank">info@lixi.com</a>
-                                    </li>
-                                    <li>
-                                        <span>تلفن:</span>
-                                        <a href="tel:021-12345678" target="_blank">021-12345678</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="footer-social">
-                                <ul>
-                                    <li>
-                                        <a href="#" target="_blank">
-                                            <i class='bx bxl-facebook'></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" target="_blank">
-                                            <i class='bx bxl-twitter'></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" target="_blank">
-                                            <i class='bx bxl-linkedin'></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" target="_blank">
-                                            <i class='bx bxl-instagram'></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="copyright-area">
-                    <p>کپی رایت 1400 ©  تمام حقوق قالب محفوظ است. طراحی توسط <a href="https://www.rtl-theme.com/author/barat/?aff=barat" target="_blank">Barat Hadian</a></p>
-                </div>
-            </div>
-        </footer>
-        <!-- End Footer -->
 
 	</div>
 </template>
 <script>
-import Loader from '@/components/loader'
+// import Loader from '@/components/loader'
+import Subscribe from '@/components/subscribe'
+import BlogPost from '@/components/blogPost'
 
 export default {
     components:{
-        Loader
+        // Loader,
+        Subscribe,
+        BlogPost
     }
 }
 </script>
