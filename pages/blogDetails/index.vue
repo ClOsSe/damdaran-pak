0fc6a58751eefd0d1bffec37fd54bd05e4ee18c0
shayan <shayanazadbakht1781@gmail.com>
2021-06-22 11:47:20 +0430

    faq add a active index for select faq

---
+++ pages/blogDetails/index.vue
@@ -0,0 +1,269 @@
+<template>
+<div dir="rtl">
+
+         <!-- Preloader -->
+        <div class="loader">
+            <Loader />
+        </div>
+        <!-- End Preloader -->
+
+     
+        <!-- Page Title -->
+        <div class="page-title-area">
+            <div class="d-table">
+                <div class="d-table-cell">
+                    <div class="container">
+                        <div class="title-item">
+                            <h2>جزئیات وبلاگ</h2>
+                            <ul>
+                                <li>
+                                    <NuxtLink to="/">صفحه اصلی</NuxtLink>
+                                </li>
+                                <li>
+                                    <i class='bx bx-chevrons-left'></i>
+                                </li>
+                                <li>
+                                    <span>جزئیات وبلاگ</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Page Title -->
+
+        <!-- Blog Details -->
+        <div class="blog-details-area ptb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                        <div class="details-item">
+                            <div class="details-img">
+                                <img src="assets/img/service-details3.jpg" alt="Details">
+                                <ul>
+                                    <li>
+                                        <i class='bx bx-user'></i>
+                                        توسط: <a href="#">مدیر</a>
+                                    </li>
+                                    <li>
+                                        <i class='bx bx-calendar-alt'></i>
+                                        20 دی 1398
+                                    </li>
+                                    <li>
+                                        <i class='bx bx-message-detail'></i>
+                                        <a href="#">نظرات (02)</a>
+                                    </li>
+                                </ul>
+                                <h2>آموزش نحوه رزرو آسان خدمات نظافتی شرکت لیکسی </h2>
+                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                                <blockquote>
+                                    "لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است."
+                                    <span>جان اسمیت</span>
+                                </blockquote>
+                                <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                            </div>
+                            <div class="details-comments">
+                                <h3>نظرات (02)</h3>
+                                <ul>
+                                    <li>
+                                        <img src="assets/img/home-one/review1.jpg" alt="Details">
+                                        <h4>جان اسمیت</h4>
+                                        <span>20 دی 1398</span>
+                                        <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد.  لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                                        <a href="#">پاسخ</a>
+                                    </li>
+                                    <li>
+                                        <img src="assets/img/home-one/review2.jpg" alt="Details">
+                                        <h4>جان اسمیت</h4>
+                                        <span>20 دی 1398</span>
+                                        <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                                        <a href="#">پاسخ</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="details-form">
+                                <h3>پاسخ دهید</h3>
+                                <span>آدرس ایمیل شما منتشر نخواهد شد. قسمتهای مورد نیاز علامت گذاری شده اند *</span>
+
+                                <form>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label>نام*</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label>ایمیل*</label>
+                                                <input type="email" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="form-group">
+                                                <label>نظر</label>
+                                                <textarea id="your-comment" rows="10" class="form-control"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <button type="submit" class="btn cmn-btn">ارسال</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="widget-area">
+                            <div class="search widget-item">
+                                <form>
+                                    <input type="text" class="form-control" placeholder="جستجو...">
+                                    <button type="submit" class="btn">
+                                        <i class='bx bx-search'></i>
+                                    </button>
+                                </form>
+                            </div>
+                            <div class="cat widget-item">
+                                <h3>دسته بندی ها</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class='bx bxs-car-wash'></i>
+                                            شستشوی خودرو
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class='bx bx-home-alt'></i>
+                                            نظافت منزل
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class='bx bx-bath'></i>
+                                            سرویس لوله کشی
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class='bx bx-building-house'></i>
+                                            نظافت محل کار
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class='bx bx-door-open'></i>
+                                            قالی شویی
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class='bx bx-home-circle'></i>
+                                            شستشوی پنجره
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="related widget-item">
+                                <h3>پست های مرتبط</h3>
+                                <div class="related-inner">
+                                    <ul class="align-items-center">
+                                        <li>
+                                            <img src="assets/img/service-details12.jpg" alt="Details">
+                                        </li>
+                                        <li>
+                                            <a href="#">نظافت محل کار</a>
+                                            <span>20 دی</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="related-inner">
+                                    <ul class="align-items-center">
+                                        <li>
+                                            <img src="assets/img/service-details13.jpg" alt="Details">
+                                        </li>
+                                        <li>
+                                            <a href="#">نظافت منزل</a>
+                                            <span>21 دی</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="related-inner">
+                                    <ul class="align-items-center">
+                                        <li>
+                                            <img src="assets/img/service-details14.jpg" alt="Details">
+                                        </li>
+                                        <li>
+                                            <a href="#">شستشوی پنجره</a>
+                                            <span>22 دی</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="newsletter widget-item">
+                                <h3>خبرنامه</h3>
+                                <form>
+                                    <input type="email" class="form-control" placeholder="آدرس ایمیل شما">
+                                    <button type="submit" class="btn cmn-btn">مشترک شدن</button>
+                                </form>
+                            </div>
+                            <div class="tags widget-item">
+                                <h3>برچسب ها</h3>
+                                <ul>
+                                    <li>
+                                        <a href="#">نظافت</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">بهداشتی</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">آژانس</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">محل کار</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">آشپزخانه</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">شرکت</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">ضد عفونی</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="call widget-item">
+                                <h3>تماس بگیرید</h3>
+                                <a href="tel:021-12345678">021-12345678</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Blog Details -->
+
+        <!-- Subscribe -->
+        <Subscribe />
+        <!-- End Subscribe -->
+
+</div>
+</template>
+<script>
+import Loader from '@/components/loader'
+import Subscribe from '@/components/subscribe'
+export default {
+    components:{
+        Loader,
+        Subscribe
+    },
+     mounted(){
+        setTimeout(() => {
+            document.querySelector(".loader").style.display="none"
+        }, 1000);
+    }
+}
+</script>
--- static/js/custom.js
+++ static/js/custom.js
@@ -50,18 +50,7 @@
 	});
 	$('select').niceSelect();
 	$('.accordion > li:eq(0) a').addClass('active').next().slideDown();
-	$('.accordion a').on('click', function(j) {
-		var dropDown = $(this).closest('li').find('p');
-		$(this).closest('.accordion').find('p').not(dropDown).slideUp();
-		if ($(this).hasClass('active')) {
-			$(this).removeClass('active');
-		} else {
-			$(this).closest('.accordion').find('a.active').removeClass('active');
-			$(this).addClass('active');
-		}
-		dropDown.stop(false, true).slideToggle();
-		j.preventDefault();
-	});
+	
 	let getDaysId = document.getElementById('days');
 	if (getDaysId !== null) {
 		const second = 1000;
