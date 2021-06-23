fd5a0c5f4ff442f8eebbdd490834fe2bafa29f1b
mohammad <mrg2195@gmail.com>
2021-06-22 20:04:47 -0700

    blog side panel created

--- pages/blogDetails/index.vue
+++ pages/blogDetails/index.vue
@@ -115,131 +115,9 @@
                             </div>
                         </div>
                     </div>
+                    <!-- side bar -->
                     <div class="col-lg-3">
-                        <div class="widget-area">
-                            <div class="search widget-item">
-                                <form>
-                                    <input type="text" class="form-control" placeholder="جستجو...">
-                                    <button type="submit" class="btn">
-                                        <i class='bx bx-search'></i>
-                                    </button>
-                                </form>
-                            </div>
-                            <div class="cat widget-item">
-                                <h3>دسته بندی ها</h3>
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <i class='bx bxs-car-wash'></i>
-                                            شستشوی خودرو
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class='bx bx-home-alt'></i>
-                                            نظافت منزل
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class='bx bx-bath'></i>
-                                            سرویس لوله کشی
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class='bx bx-building-house'></i>
-                                            نظافت محل کار
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class='bx bx-door-open'></i>
-                                            قالی شویی
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class='bx bx-home-circle'></i>
-                                            شستشوی پنجره
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="related widget-item">
-                                <h3>پست های مرتبط</h3>
-                                <div class="related-inner">
-                                    <ul class="align-items-center">
-                                        <li>
-                                            <img src="assets/img/service-details12.jpg" alt="Details">
-                                        </li>
-                                        <li>
-                                            <a href="#">نظافت محل کار</a>
-                                            <span>20 دی</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="related-inner">
-                                    <ul class="align-items-center">
-                                        <li>
-                                            <img src="assets/img/service-details13.jpg" alt="Details">
-                                        </li>
-                                        <li>
-                                            <a href="#">نظافت منزل</a>
-                                            <span>21 دی</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="related-inner">
-                                    <ul class="align-items-center">
-                                        <li>
-                                            <img src="assets/img/service-details14.jpg" alt="Details">
-                                        </li>
-                                        <li>
-                                            <a href="#">شستشوی پنجره</a>
-                                            <span>22 دی</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="newsletter widget-item">
-                                <h3>خبرنامه</h3>
-                                <form>
-                                    <input type="email" class="form-control" placeholder="آدرس ایمیل شما">
-                                    <button type="submit" class="btn cmn-btn">مشترک شدن</button>
-                                </form>
-                            </div>
-                            <div class="tags widget-item">
-                                <h3>برچسب ها</h3>
-                                <ul>
-                                    <li>
-                                        <a href="#">نظافت</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">بهداشتی</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">آژانس</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">محل کار</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">آشپزخانه</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">شرکت</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">ضد عفونی</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="call widget-item">
-                                <h3>تماس بگیرید</h3>
-                                <a href="tel:021-12345678">021-12345678</a>
-                            </div>
-                        </div>
+                        <SideColumn />
                     </div>
                 </div>
             </div>
@@ -255,10 +133,12 @@
 <script>
 import Loader from '@/components/loader'
 import Subscribe from '@/components/subscribe'
+import SideColumn from '@/components/blogSidepanel'
 export default {
     components:{
         Loader,
-        Subscribe
+        Subscribe,
+        SideColumn
     },
      mounted(){
         setTimeout(() => {
