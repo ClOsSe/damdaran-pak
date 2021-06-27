65231fdecb5176d0cb6ac20a5ebd71cd5fb77ca7
shayan <shayanazadbakht1781@gmail.com>
2021-06-27 14:10:47 +0430

    change responsive header and subscribe

--- components/subscribe/index.vue
+++ components/subscribe/index.vue
@@ -1,27 +1,48 @@
 <template>
-<div dir="rtl">
+  <div dir="rtl">
     <div class="subscribe-area">
-            <div class="subscribe-wrap" >
-                <div class="container">
-                    <div class="row align-items-center" >
-                        <div class="col-lg-6" >
-                            <div class="section-title" style="text-align:right">
-                                <h2>در خبرنامه ما عضو شوید</h2>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <form class="newsletter-form" data-toggle="validator">
-                                <input type="email" class="form-control" placeholder="آدرس ایمیل شما" name="EMAIL" required autocomplete="off">
-        
-                                <button class="btn cmn-btn" type="submit">
-                                    مشترک شدن
-                                </button>
-                                <div id="validator-newsletter" class="form-result"></div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+      <div class="subscribe-wrap px-1">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="section-title" style="text-align:right">
+                <h2>در خبرنامه ما عضو شوید</h2>
+              </div>
             </div>
+            <div class="col-lg-6 col-12 py-4 px-2">
+              <form class=" newsletter-form" data-toggle="validator">
+               
+               <div class="row">
+                 
+                   <div class="col-12 col-md my-2 my-md-0 ">
+                         <input
+                    type="email"
+                    class="form-control w-100"
+                    placeholder="آدرس ایمیل شما"
+                    name="EMAIL"
+                    required
+                    autocomplete="off"
+                  />
+                   </div>
+                     <div class=" col-12 col-md-auto d-flex">
+                       <button class="btn button-submitt" type="submit">
+                    مشترک شدن
+                  </button>
+                   </div>
+               </div>
+              </form>
+            </div>
+          </div>
         </div>
-</div>
+      </div>
+    </div>
+  </div>
 </template>
+<style scoped>
+
+.button-submitt{
+    color: white;
+    background-color: #fdb139;
+    border-radius: 30px;
+}
+</style>
