77c33506935c7baa45a27f9caf605c397641c714
mohammad <mrg2195@gmail.com>
2021-07-03 05:05:27 -0700

    iframe tag updated

--- components/map/index.vue
+++ components/map/index.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-         <div class="container-fluid">
-                <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59843174.53779284!2d62.17507173408573!3d23.728204508550373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3663f18a24cbe857%3A0xa9416bfcd3a0f459!2sAsia!5e0!3m2!1sen!2sbd!4v1598214277553!5m2!1sen!2sbd" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-            </div>
+  <div>
+    <div class="container-fluid">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d572.3772676206215!2d51.28005867627238!3d35.75437398712922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8dfd2d4b399423%3A0x3434fc29a1613eb0!2z2q_ZiNi02Kog2q_ZiNiz2YHZhtiv!5e0!3m2!1sen!2sus!4v1625312913929!5m2!1sen!2sus"
+        id="map"
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
     </div>
+  </div>
 </template>
