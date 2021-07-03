b0285cbc03e86b981647100f024670e91d0721a1
mohammad <mrg2195@gmail.com>
2021-07-03 05:35:59 -0700

    map updated

--- components/map/index.vue
+++ components/map/index.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="container-fluid">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d572.3772676206215!2d51.28005867627238!3d35.75437398712922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8dfd2d4b399423%3A0x3434fc29a1613eb0!2z2q_ZiNi02Kog2q_ZiNiz2YHZhtiv!5e0!3m2!1sen!2sus!4v1625312913929!5m2!1sen!2sus"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58147.750423248595!2d51.36438131984859!3d35.7573244117374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQ1JzE2LjAiTiA1McKwMTYnNDMuMSJF!5e0!3m2!1sen!2s!4v1625315610924!5m2!1sen!2s"
         id="map"
-        aria-hidden="false"
-        tabindex="0"
+        allowfullscreen=""
+        loading="lazy"
       ></iframe>
     </div>
   </div>
